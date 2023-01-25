c45fcdc600e00f893b937035fefebb9db075b344
bill6653 <billcrs512@gmail.com>
2023-01-25 15:56:26 -0600

    added if, else statements for timeNow function, functions to get items stored locally with save button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,10 +25,36 @@ function timeTracker() {
     */
     $(".time-block").each(function () {
     var timePast = parseInt($(this).attr("id").split("h")[1]);
-
-})
     
+    /* if statements that checks the time and changes the classes based off (timeNow) function, changes the class to future,
+    present or past also links to my css to change the colors of the time slots
+    */
+    if (timePast < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (timePast === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }})}
 
-}
+    //functions to get items stored locally in each description that use the save button
+    $("#h9 .description").val(localStorage.getItem("h9"));
+    $("#h10 .description").val(localStorage.getItem("h10"));
+    $("#h11 .description").val(localStorage.getItem("h11"));
+    $("#h12 .description").val(localStorage.getItem("h12"));
+    $("#h13 .description").val(localStorage.getItem("h13"));
+    $("#h14 .description").val(localStorage.getItem("h14"));
+    $("#h15 .description").val(localStorage.getItem("h15"));
+    $("#h16 .description").val(localStorage.getItem("h16"));
+    $("#h17 .description").val(localStorage.getItem("h17"));
 
+    timeTracker();
 })
